5e38a217af2846dc0ad9f2cc0d7ac90de8791688
carla.canovas <carla.canovas@est.fib.upc.edu>
2022-12-10 22:03:46 +0100

    #205 #188 Working on users page

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,6 +1,31 @@
 import React from "react";
 import "../App.css";
+import { Button, ButtonGroup, ButtonToolbar } from "react-bootstrap";
 
 export default function Users() {
-  return <h1 className="users">USERS</h1>;
+  const handleClick = () => null;
+
+  return (
+    <div className="users-container">
+      <h1 className="users-title">USERS</h1>
+      <ButtonToolbar aria-label="Toolbar with button groups">
+        <ButtonGroup style={{ marginLeft: "1rem" }} aria-label="Second group">
+          <Button
+            style={{ background: "#59DE87" }}
+            variant="secondary"
+            onClick={handleClick}
+          >
+            Block user
+          </Button>
+          <Button
+            style={{ background: "#59DE87" }}
+            variant="secondary"
+            onClick={handleClick}
+          >
+            Update user
+          </Button>
+        </ButtonGroup>
+      </ButtonToolbar>
+    </div>
+  );
 }
