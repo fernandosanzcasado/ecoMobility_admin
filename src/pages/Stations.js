6c026dd019e73b15634816f925b3273b13f6a9a4
carla.canovas <carla.canovas@est.fib.upc.edu>
2022-12-10 20:23:09 +0100

    #189 Starting ToolBar for stations page

--- src/pages/Stations.js
+++ src/pages/Stations.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "../App.css";
+import { Button, ButtonGroup, ButtonToolbar } from "react-bootstrap";
 
 export default function Sations() {
-  return <h1 className="stations">STATIONS</h1>;
+  return (
+    <div className="stations-container">
+      <h1 className="stations-title">STATIONS</h1>
+      <ButtonToolbar aria-label="Toolbar with button groups">
+        <ButtonGroup style={{ marginLeft: "50rem" }} aria-label="First group">
+          <Button variant="secondary">Left</Button>
+          <Button variant="secondary">Middle</Button>
+        </ButtonGroup>
+        <ButtonGroup style={{ marginLeft: "1rem" }} aria-label="Second group">
+          <Button variant="secondary">Left</Button>
+        </ButtonGroup>
+      </ButtonToolbar>
+    </div>
+  );
 }
