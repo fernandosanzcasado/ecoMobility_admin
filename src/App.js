29ff283612d845e1bc5863b6bd3835a851aae15a
carla.canovas <carla.canovas@est.fib.upc.edu>
2022-12-10 19:20:41 +0100

    #180 Creating cards in home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import NavBar from "./components/NavBar";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Users from "./pages/Users";
 import Stations from "./pages/Stations";
