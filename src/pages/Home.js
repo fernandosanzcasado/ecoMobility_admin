39db0ec277103a35903f99570245ac9440256d7a
carlaCanovas <ccanovas07@gmail.com>
2022-12-26 13:11:46 +0100

    #180 Adding buttons to change language

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,67 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "../App.css";
+import { useTranslation } from "react-i18next";
+import "../i18n.js";
+
+import English from "../images/BanderaInglesa.jpg";
+import Spanish from "../images/BanderaEspañola.jpg";
+import Catalan from "../images/BanderaCatalana.jpg";
 
 export default function Home() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   return (
-    <div className="home-container">
-      <Card
-        style={{ width: "18rem", background: "#59DE87", marginRight: "8rem" }}
-      >
-        <Card.Body>
-          <Card.Title>Total Users</Card.Title>
-          <Card.Text>Number of registered users</Card.Text>
-        </Card.Body>
-      </Card>
-      <Card
-        style={{ width: "18rem", background: "#59DE87", marginRight: "8rem" }}
-      >
-        <Card.Body>
-          <Card.Title>Total electric stations</Card.Title>
-          <Card.Text>Number of electric stations</Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: "18rem", background: "#59DE87" }}>
-        <Card.Body>
-          <Card.Title>Total bike stations</Card.Title>
-          <Card.Text>Number of bike stations</Card.Text>
-        </Card.Body>
-      </Card>
+    <div>
+      <div className="cards-container">
+        <Card
+          style={{ width: "18rem", background: "#59DE87", marginRight: "8rem" }}
+        >
+          <Card.Body>
+            <Card.Title>Total Users</Card.Title>
+            <Card.Text>Number of registered users</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card
+          style={{ width: "18rem", background: "#59DE87", marginRight: "8rem" }}
+        >
+          <Card.Body>
+            <Card.Title>Total electric stations</Card.Title>
+            <Card.Text>Number of electric stations</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "18rem", background: "#59DE87" }}>
+          <Card.Body>
+            <Card.Title>Total bike stations</Card.Title>
+            <Card.Text>Number of bike stations</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="flagsView">
+        <img
+          className="flags"
+          src={English}
+          onClick={() => {
+            changeLanguage("en");
+          }}
+        ></img>
+        <img
+          className="flags"
+          src={Spanish}
+          onClick={() => {
+            changeLanguage("cast");
+          }}
+        ></img>
+        <img
+          className="flags"
+          src={Catalan}
+          onClick={() => {
+            changeLanguage("cat");
+          }}
+        ></img>
+      </div>
     </div>
   );
 }
