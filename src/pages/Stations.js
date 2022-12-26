88cad2d7f33778055ec40224caefa37f55ba86aa
carlaCanovas <ccanovas07@gmail.com>
2022-12-26 12:42:29 +0100

    #189 finishing stations interface

--- src/pages/Stations.js
+++ src/pages/Stations.js
@@ -1,54 +1,33 @@
 import React from "react";
 import "../App.css";
 import { Button, ButtonGroup, ButtonToolbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function Stations() {
-  const handleClick = () => null;
+  const navigate = useNavigate();
+  const handleClickElectric = () => navigate("/stations/electricStations");
+  const handleClickBike = () => navigate("/stations/bikeStations");
 
   return (
     <div className="stations-container">
       <h1 className="stations-title">STATIONS</h1>
-      <Link to="/stations/electricStations" className="nav-links">
-        Home
-      </Link>
-      {/* <ButtonToolbar aria-label="Toolbar with button groups"> */}
       <ButtonGroup aria-label="First group">
         <Button
           style={{ background: "#59DE87" }}
           variant="secondary"
-          onClick={handleClick}
+          onClick={handleClickElectric}
         >
           Electric stations
         </Button>
-        <Button style={{ background: "#59DE87" }} variant="secondary">
+        <Button
+          style={{ background: "#59DE87" }}
+          variant="secondary"
+          onClick={handleClickBike}
+        >
           Bike stations
         </Button>
       </ButtonGroup>
-      {/* <ButtonGroup style={{ marginLeft: "1rem" }} aria-label="Second group">
-          <Button
-            style={{ background: "#59DE87" }}
-            variant="secondary"
-            onClick={handleClick}
-          >
-            Add station
-          </Button>
-          <Button
-            style={{ background: "#59DE87" }}
-            variant="secondary"
-            onClick={handleClick}
-          >
-            Delete station
-          </Button>
-          <Button
-            style={{ background: "#59DE87" }}
-            variant="secondary"
-            onClick={handleClick}
-          >
-            Update station
-          </Button>
-        </ButtonGroup> */}
-      {/* </ButtonToolbar> */}
     </div>
   );
 }
