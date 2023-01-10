e74ff9df2bfcefa177b2eec851cbabebbcc0329f
carlaCanovas <ccanovas07@gmail.com>
2023-01-10 18:27:53 +0100

    #252 necessary components for chat

---
+++ src/components/Message.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./App.css";
+
+export default function Message() {
+  return <h1>Message</h1>;
+}
---
+++ src/pages/Chat.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./App.css";
+
+export default function Chat() {
+  return <h1>Chat</h1>;
+}
