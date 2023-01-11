a5108417970a514b77a23b2759d704432ee645ec
carlaCanovas <ccanovas07@gmail.com>
2023-01-11 10:30:38 +0100

    #252 Message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,53 @@
 import React from "react";
 import "../App.css";
 
-export default function Message() {
-  return <h1>Message</h1>;
+import Avatar from "@mui/material/Avatar";
+import { deepPurple } from "@mui/material/colors";
+
+export default function Message({ respuesta, msg, sentMsg }) {
+  return (
+    <div>
+      {respuesta && (
+        <div style={styles.respuestaBox}>
+          <Avatar sx={{ bgcolor: deepPurple[500] }}>:)</Avatar>
+          <label style={styles.respuesta}>{msg}</label>
+        </div>
+      )}
+      {sentMsg && (
+        <label style={styles.msgbox}>
+          <label style={styles.msgtext}>{msg}</label>
+        </label>
+      )}
+    </div>
+  );
 }
+
+const styles = StyleSheet.create({
+  msgbox: {
+    backgroundColor: "green",
+    maxWidth: "70%",
+    borderRadius: 10,
+    padding: 5,
+    alignSelf: "flex-end",
+    marginVertical: 5,
+    marginHorizontal: 5,
+  },
+  msgtext: { color: "#fff", fontSize: 16 },
+  respuesta: {
+    color: "black",
+    fontSize: 16,
+    marginRight: 30,
+  },
+  respuestaBox: {
+    backgroundColor: "white",
+    maxWidth: "75%",
+    borderRadius: 10,
+    paddingRight: 10,
+    alignSelf: "flex-start",
+    marginVertical: 5,
+    marginHorizontal: 5,
+    borderWidth: 0.5,
+    borderColor: "grey",
+    flexDirection: "row",
+  },
+});
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "../App.css";
 
 export default function Chat() {
   return <h1>Chat</h1>;
